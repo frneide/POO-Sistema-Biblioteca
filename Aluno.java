c5f3de171d4339b87a88ee7c9fddf35ab9821d88
João Gabriel Frota de Moura Santos <frotaj22@gmail.com>
2026-01-19 16:02:10 -0300

    Update Aluno.java

--- Aluno.java
+++ Aluno.java
@@ -13,4 +13,9 @@ public class Aluno extends Usuario {
     public void setMatricula(String matricula) { this.matricula = matricula; }
     public String getCurso() { return curso; }
     public void setCurso(String curso) { this.curso = curso; }
+
+        @Override
+    public String toCSV() {
+        return "ALUNO;" + getId() + ";" + getNome() + ";" + getEmail() + ";" + getDataNasc() + ";" + getTelefone() + ";" + matricula + ";" + curso; //
+    }
 }
